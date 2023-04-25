4148b47ba9fa87e058de51f84b11d27e439ec750
CamilaDanielaBanaresCarrasco <camilabanares.informaticos@gmail.com>
2023-04-24 23:15:53 -0400

    se agrega funcionalidad de oscurecer y eh indicar lucar y descripcion

--- Asset/Js/index.js
+++ Asset/Js/index.js
@@ -1,17 +1,37 @@
 function evaluarOpcion(opcion) {
-  // Ocultar todas las imágenes
-  $('.imagen').hide();
+  // Ocultar todas las imágenes excepto la de Trekking
+  $('.imagen').not('#imagenTrekking').hide();
 
   switch(opcion) {
     case 'trekking':
       console.log('Has seleccionado Trekking');
       var fila = $("<section> <br>");
+      var fila2 = $("<section> <br>");
+      
       fila.append($("<div>")
-        .addClass('imagen')
-        .attr('id', 'imagenTrekking')
-        .html("<img src='https://wildhawk.in/wp-content/uploads/2019/05/trekking-3.jpg' width='800px' height='500px'>")
+      .addClass('imagen')
+      .attr('id', 'imagenTrekking')
+      .html("<img src='https://wildhawk.in/wp-content/uploads/2019/05/trekking-3.jpg' width='800px' height='500px'>" +
+          "<div class='overlay'>" +
+          "<h1 class='text fs-1'>TREKKING</h1> <h2 class='text2 fs-3'> Volcan Villarrica <h2>" +
+          "</div>")
+  );
+  
+
+
+      fila.append($("<div>")
+      .attr('id', 'descripcionTrekking')
+      .html("<div class='container border bg-light text-start col-6 mt-5 ' ><p class=' text-justify m-0 '>Modalidad de excursismo que consiste en  recorrer a pie largas distancias o zonas determinadas, generalmente de alta montala y poco frecuentadas por el turismo convencional.</p><a type='button' class='btn btn-link p-0'>Conocer más</a> <br> <button type='button' class='btn btn-light'>Agendar actividad</button></div>")
       );
+
+
+      $("#descripccion").append(fila2);
       $("#imagen").append(fila);
+
+      // Mostrar imagen y descripción de Trekking por defecto
+      $('#imagenTrekking').show();
+      $('#descripcionTrekking').show();
+   
       break;
     case 'natacion':
       console.log('Has seleccionado Natación');
@@ -19,8 +39,11 @@ function evaluarOpcion(opcion) {
       fila.append($("<div>")
         .addClass('imagen')
         .attr('id', 'imagenNatacion')
-        .html("<img src='https://concepto.de/wp-content/uploads/2019/06/natacion-e1562943144215.jpg' width='800px' height='500px'>")
-      );
+        .html("<img src='https://concepto.de/wp-content/uploads/2019/06/natacion-e1562943144215.jpg' width='800px' height='500px'>" +
+        "<div class='overlay'>" +
+        "<h1 class='text fs-1'>NATACION</h1> <h2 class='text2 fs-3'> Volcan Villarrica <h2>" +
+        "</div>")
+);
       $("#imagen").append(fila);
       break;
     case 'ferry':
